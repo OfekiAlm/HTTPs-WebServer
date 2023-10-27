765cf0285375fe7c7d656b8fb78e03e9de24f82f
Ofek Almog <ofekalm100@gmail.com>
2023-10-27 01:46:50 +0100

    implemented TLS layer

--- ssl/ssl.c
+++ ssl/ssl.c
@@ -6,3 +6,31 @@ void initialize_ssl(){
     SSL_load_error_strings();
     OpenSSL_add_ssl_algorithms();
 }
+
+SSL_CTX *create_context() {
+    const SSL_METHOD *method;
+    SSL_CTX *ctx;
+
+    method = SSLv23_server_method();
+    ctx = SSL_CTX_new(method);
+    if (!ctx) {
+        perror("Unable to create SSL context");
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    return ctx;
+}
+
+void configure_context(SSL_CTX *ctx) {
+    SSL_CTX_use_certificate_file(ctx, "cert.pem", SSL_FILETYPE_PEM);
+    SSL_CTX_use_PrivateKey_file(ctx, "key.pem", SSL_FILETYPE_PEM);
+
+    if (!SSL_CTX_check_private_key(ctx)) {
+        fprintf(stderr, "Private key does not match the public certificate\n");
+        exit(EXIT_FAILURE);
+    }
+}
+//SSL *new_ssl(SSL_CTX *ctx){
+//    SSL_new(ctx);
+//}
